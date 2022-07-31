74275aec5c0628a5cc7cbd1789a4355caa6e1863
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-01 00:20:52 +0700

    adding some comment info and change var to const identifier

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 // Import Route
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// reference use: https://www.npmjs.com/package/debug
+// reference use: https://www.npmjs.com/package/morgan
 app.use(logger('dev')); // A tiny node debugging utility modeled after node core's debugging technique.
 
 // Data sending from FORM
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cookieParser());
+// reference use: https://www.npmjs.com/package/cookie-parser
+app.use(cookieParser()); // Parse Cookie header and populate req.cookies with an object keyed by the cookie names. Optionally you may enable signed cookie support by passing a secret string, which assigns req.secret so it may be used by other middleware.
 
 // STATIC FILES can be seen as PUBLIC
 app.use(express.static(path.join(__dirname, 'public')));
