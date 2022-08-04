8efa8a6e39924afc474eed2ae53fc6ca5b6b082d
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-04 16:16:52 +0700

    remove utils folder, and change MONGO_URI to .env file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
@@ -13,9 +14,8 @@ const app = express()
 // Mongoose Connection Setup
   // Import Connection
   const db = require('./utils/atlas-connection')
-  const atlasPassword = require('./utils/auth/atlas-password')
   const mongoose = require('mongoose')
-  mongoose.connect(atlasPassword, {
+  mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
   })
