2719617e9f43c8847c3381942a9609ef769a39a5
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-16 03:56:00 +0700

    Display detail page for a specific genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -4,3 +4,8 @@ const Genre = require('../models/genre')
 exports.genre_list = (req, res) => {
     res.send('Genre List')
 }
+
+// Display detail page for a specific genre
+exports.genre_detail = (req, res) => {
+    res.send(`Genre Detail: ${req.params.id}`)
+}
