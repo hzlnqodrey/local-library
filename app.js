1808c03268150744592879ea7a55245ed202c36b
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-16 02:09:32 +0700

    delete imported db files config on utils folder

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const app = express()
 
 // Mongoose Connection Setup
   // Import Connection
-  const db = require('./utils/atlas-connection')
   const mongoose = require('mongoose')
   mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
