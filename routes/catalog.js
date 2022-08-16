d008a36fdb7242da5bfef5e9da26859319fa2100
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-16 12:06:18 +0700

    Import/Require Controller modules.

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,3 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+// Import/Require Controller modules.
+const author_controller = require('../controllers/authorController')
+const book_controller = require('../controllers/bookController')
+const bookinstance_controller = require('../controllers/bookinstanceController')
+const genre = require('../controllers/genreController')
