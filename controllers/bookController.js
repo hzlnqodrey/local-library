da09eef777418f7e3454eba6cd6adc28e244ac78
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-08-20 20:19:35 +0700

    add author, genre, bookinstance models to bookcontroller, because index (main page) is there

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,7 @@
-const Book = require('../models/book')
+const Book = require('../models/book');
+const Author = require('../models/author');
+const Genre = require('../models/genre');
+const BookInstance = require('../models/bookinstance');
 
 // Index Site Home Page
 exports.index = (req, res) => {
